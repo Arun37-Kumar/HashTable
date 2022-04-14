0a298cfc4da2c32d5a33a8a9d7a304d0663699ab
Arun37-Kumar <arun007624@gmail.com>
2022-04-14 16:49:59 +0530

    commit

---
+++ HashTable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash Table Problem!\n---------------");
+            MyMapNode<string, string> hashAdd = new MyMapNode<string, string>(5);
+
+            //Add data
+            hashAdd.Add("0", "To");
+            hashAdd.Add("1", "be");
+            hashAdd.Add("2", "or");
+            hashAdd.Add("3", "not");
+            hashAdd.Add("4", "to");
+            hashAdd.Add("5", "be");
+
+            //Get Value and Display
+            string hash5 = hashAdd.Get("5");
+            Console.WriteLine("5th index  value : {0}", hash5);
+
+            //Get Value and Display
+            string hash2 = hashAdd.Get("2");
+            Console.WriteLine("2nd index  value : {0}", hash2);
+            Console.ReadLine();
+        }
+    }
+}
